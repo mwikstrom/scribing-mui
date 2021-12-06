5704a2d1393f21c9c6bbd58bf71f2b1abb6e5bc5
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2021-12-06 13:10:35 +0100

    palette type

--- stories/FlowEditorToolbar.stories.tsx
+++ stories/FlowEditorToolbar.stories.tsx
@@ -1,24 +1,36 @@
-import React, { useMemo, useState } from "react";
+import React, { FC, useMemo, useState } from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { FlowEditorToolbar } from "../src/FlowEditorToolbar";
-import { createTheme, MuiThemeProvider } from "@material-ui/core";
+import { createTheme, MuiThemeProvider, Paper } from "@material-ui/core";
 import { FlowEditor, FlowEditorController } from "scribing-react";
 
-export default {
-    title: "FlowEditorToolbar",
-    component: FlowEditorToolbar,
-} as ComponentMeta<typeof FlowEditorToolbar>;
-  
-const Template: ComponentStory<typeof FlowEditorToolbar> = args => {
+interface StoryProps {
+    dark?: boolean;
+}
+
+const Story: FC<StoryProps> = props => {
+    const { dark } = props;
     const [controller, setController] = useState<FlowEditorController | null>(null);
-    const theme = useMemo(() => createTheme(), []);
+    const theme = useMemo(() => createTheme({ palette: { type: dark ? "dark" : "light" }  }), [dark]);
     return (
         <MuiThemeProvider theme={theme}>
-            <FlowEditorToolbar {...args} controller={controller}/>
-            <FlowEditor onControllerChange={setController}/>
+            <Paper>
+                <FlowEditorToolbar controller={controller}/>
+                <FlowEditor onControllerChange={setController} autoFocus/>
+            </Paper>
         </MuiThemeProvider>
     );
 };
 
-export const Default = Template.bind({});
-Default.args = {};
+export default {
+    title: "FlowEditorToolbar",
+    component: Story,
+} as ComponentMeta<typeof Story>;
+
+const Template: ComponentStory<typeof Story> = args => <Story {...args}/>;
+
+export const Light = Template.bind({});
+Light.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = { dark: true };
