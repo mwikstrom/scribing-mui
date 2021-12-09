3dd6052dd121b0b12aa1229eb46ba0f9c22e2bde
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2021-12-09 01:01:48 +0100

    use option button

--- src/tools/BoxVariantSelector.tsx
+++ src/tools/BoxVariantSelector.tsx
@@ -1,13 +1,14 @@
-import { Menu, MenuItem, Theme, Typography } from "@material-ui/core";
+import { Theme, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
-import { mdiCheck, mdiMenuDown, mdiTextBoxOutline } from "@mdi/js";
+import { mdiMenuDown, mdiTextBoxOutline } from "@mdi/js";
 import { Icon } from "@mdi/react";
 import clsx from "clsx";
-import React, { FC, useCallback, useMemo, useState } from "react";
+import React, { FC, useCallback, useMemo } from "react";
 import { BoxVariant, BOX_VARIANTS } from "scribing";
 import { FlowEditorController } from "scribing-react";
 import { useMaterialFlowLocale } from "../MaterialFlowLocale";
-import { ToolButton, ToolButtonProps } from "./ToolButton";
+import { OptionButton } from "./OptionButton";
+import { ToolButtonProps } from "./ToolButton";
 
 export interface BoxVariantSelectorProps extends ToolButtonProps {
     controller?: FlowEditorController | null;
@@ -15,61 +16,37 @@ export interface BoxVariantSelectorProps extends ToolButtonProps {
 
 export const BoxVariantSelector: FC<BoxVariantSelectorProps> = props => {
     const { controller, ...rest } = props;
-    const current = useMemo(() => controller?.getBoxStyle()?.variant, [controller]);
-    const [isMenuOpen, setMenuOpen] = useState(false);
-    const [buttonRef, setButtonRef] = useState<HTMLElement | null>(null);
-    const openMenu = useCallback(() => setMenuOpen(true), []);
-    const closeMenu = useCallback(() => setMenuOpen(false), []);
-    const applyVariant = useCallback((variant: BoxVariant) => {
-        closeMenu();
-        controller?.formatBox("variant", variant);
-    }, [controller, closeMenu]);
-    const classes = useStyles();
+    const selected = useMemo(() => controller?.getBoxStyle()?.variant, [controller]);
     const locale = useMaterialFlowLocale();
+    const getOptionLabel = useCallback((value: BoxVariant) => locale[`box_${value}`], [locale]);
+    const onOptionSelected = useCallback((variant: BoxVariant) => {
+        controller?.formatBox("variant", variant);
+    }, [controller]);
+    const classes = useStyles();
     return (
-        <>
-            <ToolButton
-                {...rest} 
-                ref={setButtonRef}
-                startIcon={<Icon size={0.75} path={mdiTextBoxOutline}/>}
-                endIcon={<Icon size={1} path={mdiMenuDown}/>}
-                onClick={openMenu}
-                disabled={!controller}
-                children={(
-                    <div className={classes.label}>
-                        {BOX_VARIANTS.map(variant => (
-                            <Typography
-                                key={variant}
-                                variant="body2"
-                                component="div"
-                                className={clsx(classes.labelItem, current !== variant && classes.inactiveLabelItem)}
-                                children={locale[`box_${variant}`]}
-                            />
-                        ))}            
-                    </div>
-                )}
-            />
-            <Menu open={isMenuOpen} anchorEl={buttonRef} onClose={closeMenu}>
-                {BOX_VARIANTS.map(variant => (
-                    <MenuItem
-                        key={variant}
-                        disableGutters
-                        selected={current === variant}
-                        onClick={() => applyVariant(variant)}
-                        children={(
-                            <div className={classes.menuItem}>
-                                <Icon
-                                    className={classes.menuIcon}
-                                    size={0.75}
-                                    path={current === variant ? mdiCheck : ""}
-                                />
-                                <Typography variant="body2">{locale[`box_${variant}`]}</Typography>
-                            </div>
-                        )}
-                    />
-                ))}
-            </Menu>
-        </>
+        <OptionButton
+            {...rest} 
+            startIcon={<Icon size={0.75} path={mdiTextBoxOutline}/>}
+            endIcon={<Icon size={1} path={mdiMenuDown}/>}
+            disabled={!controller}
+            selected={selected}
+            options={BOX_VARIANTS}
+            onOptionSelected={onOptionSelected}
+            getOptionLabel={getOptionLabel}
+            children={(
+                <div className={classes.label}>
+                    {BOX_VARIANTS.map(variant => (
+                        <Typography
+                            key={variant}
+                            variant="body2"
+                            component="div"
+                            className={clsx(classes.labelItem, selected !== variant && classes.inactiveLabelItem)}
+                            children={locale[`box_${variant}`]}
+                        />
+                    ))}            
+                </div>
+            )}
+        />
     );
 };
 
@@ -85,15 +62,5 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
     inactiveLabelItem: {
         height: 0,
-    },
-    menuItem: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        paddingLeft: theme.spacing(1),
-        paddingRight: theme.spacing(4),
-    },
-    menuIcon: {
-        marginRight: theme.spacing(1),
     },
 }));
