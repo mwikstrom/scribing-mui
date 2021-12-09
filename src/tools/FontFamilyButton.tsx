684a542b0ce99af4d153461b2fddff9fffc5046f
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2021-12-09 02:12:31 +0100

    use option button

--- src/tools/FontFamilyButton.tsx
+++ src/tools/FontFamilyButton.tsx
@@ -1,12 +1,11 @@
-import { Menu, MenuItem, Theme, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
-import { mdiCheck, mdiFormatFont } from "@mdi/js";
+import { mdiFormatFont } from "@mdi/js";
 import { Icon } from "@mdi/react";
-import React, { FC, useCallback, useMemo, useState } from "react";
+import React, { FC, useCallback, useMemo } from "react";
 import { FontFamily, FONT_FAMILIES } from "scribing";
 import { FlowEditorController } from "scribing-react";
 import { useMaterialFlowLocale } from "../MaterialFlowLocale";
-import { ToolButton, ToolButtonProps } from "../components/ToolButton";
+import { ToolButtonProps } from "../components/ToolButton";
+import { OptionButton } from "../components/OptionButton";
 
 export interface FontFamilyButtonProps extends ToolButtonProps {
     controller?: FlowEditorController | null;
@@ -14,59 +13,19 @@ export interface FontFamilyButtonProps extends ToolButtonProps {
 
 export const FontFamilyButton: FC<FontFamilyButtonProps> = props => {
     const { controller, ...rest } = props;
-    const current = useMemo(() => controller?.getFontFamily(), [controller]);
-    const [isMenuOpen, setMenuOpen] = useState(false);
-    const [buttonRef, setButtonRef] = useState<HTMLElement | null>(null);
-    const openMenu = useCallback(() => setMenuOpen(true), []);
-    const closeMenu = useCallback(() => setMenuOpen(false), []);
-    const applyFont = useCallback((font: FontFamily) => {
-        closeMenu();
-        controller?.setFontFamily(font);
-    }, [controller, closeMenu]);
-    const classes = useStyles();
+    const selected = useMemo(() => controller?.getFontFamily(), [controller]);
+    const onOptionSelected = useCallback((font: FontFamily) => controller?.setFontFamily(font), [controller]);
     const locale = useMaterialFlowLocale();
+    const getOptionLabel = useCallback((value: FontFamily) => locale[`font_family_${value}` as const], [locale]);
     return (
-        <>
-            <ToolButton
-                {...rest} 
-                ref={setButtonRef}
-                onClick={openMenu}
-                disabled={!controller}
-                children={<Icon size={1} path={mdiFormatFont}/>}
-            />
-            <Menu open={isMenuOpen} anchorEl={buttonRef} onClose={closeMenu}>
-                {FONT_FAMILIES.map(font => (
-                    <MenuItem
-                        key={font}
-                        disableGutters
-                        selected={current === font}
-                        onClick={() => applyFont(font)}
-                        children={(
-                            <div className={classes.menuItem}>
-                                <Icon
-                                    className={classes.menuIcon}
-                                    size={0.75}
-                                    path={current === font ? mdiCheck : ""}
-                                />
-                                <Typography variant="body2">{locale[`font_family_${font}` as const]}</Typography>
-                            </div>
-                        )}
-                    />
-                ))}
-            </Menu>
-        </>
+        <OptionButton
+            {...rest} 
+            disabled={!controller}
+            options={FONT_FAMILIES}
+            selected={selected}
+            onOptionSelected={onOptionSelected}
+            getOptionLabel={getOptionLabel}
+            children={<Icon size={1} path={mdiFormatFont}/>}
+        />
     );
 };
-
-const useStyles = makeStyles((theme: Theme) => ({
-    menuItem: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        paddingLeft: theme.spacing(1),
-        paddingRight: theme.spacing(4),
-    },
-    menuIcon: {
-        marginRight: theme.spacing(1),
-    },
-}));
