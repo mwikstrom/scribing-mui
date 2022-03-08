ab849213807985b3138626564a95e8dc3f2eb7e1
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2022-03-08 11:45:43 +0100

    enhance mui button styling

--- src/components/DefaultButton.tsx
+++ src/components/DefaultButton.tsx
@@ -1,11 +1,12 @@
 import clsx from "clsx";
-import { Button, ButtonProps, CircularProgress, Fade, Theme } from "@material-ui/core";
+import { alpha, Button, ButtonProps, CircularProgress, Fade, Theme } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import React, { forwardRef, useCallback } from "react";
-import { BoxVariant, FlowColor } from "scribing";
+import { BoxVariant } from "scribing";
 import { DefaultScribingComponents, ScribingButtonProps} from "scribing-react";
 import { mdiAlert } from "@mdi/js";
 import { Icon } from "@mdi/react";
+import { PaletteColor } from "@material-ui/core/styles/createPalette";
 
 export const DefaultButton = forwardRef<HTMLElement, ScribingButtonProps>((props, outerRef) => {
     const {
@@ -34,9 +35,8 @@ export const DefaultButton = forwardRef<HTMLElement, ScribingButtonProps>((props
 
     const classes = useStyles();
     const buttonVariant = boxVariant && ButtonVariantMapping[boxVariant];
-    const buttonColor = ButtonColorMapping[boxColor || "default"];
 
-    if (buttonVariant && buttonColor) {
+    if (buttonVariant) {
         return (
             <Button
                 {...otherProps}
@@ -44,13 +44,14 @@ export const DefaultButton = forwardRef<HTMLElement, ScribingButtonProps>((props
                 href={href || ""}
                 disabled={disabled}
                 variant={buttonVariant}
-                color={buttonColor}
+                color={undefined}
                 fullWidth={!inline}
                 className={clsx(
                     classes.root,
-                    error && classes.error,
+                    error && classes.interactionFailed,
+                    classes[`${boxColor || "default"}Color`],
                 )}
-                startIcon={error && <Icon size={1} path={mdiAlert}/>}
+                startIcon={<Icon size={1} path={mdiAlert}/>}
                 children={(
                     <>
                         {pending && (
@@ -81,31 +82,102 @@ const ButtonVariantMapping: Partial<Record<BoxVariant, ButtonProps["variant"]>> 
     contained: "contained",
 };
 
-const ButtonColorMapping: Partial<Record<FlowColor, ButtonProps["color"]>> = {
-    default: "default",
-    primary: "primary",
-    secondary: "secondary",
-};
-
 const useStyles = makeStyles((theme: Theme) => ({
     root: {
         marginRight: theme.spacing(1),
         marginTop: theme.spacing(0.5),
         marginBottom: theme.spacing(0.5),
+        transition: theme.transitions.create(["color", "background-color", "border-color"]),
+        "& .MuiButton-startIcon": {
+            transition: theme.transitions.create(["width", "margin-left", "margin-right", "opacity"]),
+            width: 0,
+            marginLeft: 0,
+            marginRight: 0,
+            opacity: 0,
+        },
+        "&$interactionFailed .MuiButton-startIcon": {
+            width: theme.spacing(3),
+            marginLeft: theme.spacing(-0.5),
+            marginRight: theme.spacing(1),
+            opacity: 1,
+        },
         "& .ScribingTextSegment-root, & .ScribingDynamicText-root > *": {
             color: "inherit !important",
             fontFamily: "inherit !important",
             fontSize: "inherit !important",
             fontWeight: "inherit !important",
-        }
-    },    
-    error: {
-        color: `${theme.palette.error.main} !important`,
-        borderColor: `${theme.palette.error.main} !important`,
+        },
+        "&$defaultColor": decorateColorStyle({}, theme.palette),
+        "&$subtleColor": decorateColorStyle({
+            color: theme.palette.text.secondary,
+            "&.MuiButton-outlined": {
+                borderColor: theme.palette.type === "light" ? "rgba(0, 0, 0, 0.18)" : "rgba(255, 255, 255, 0.18)",
+            },
+            "&.MuiButton-contained": {
+                backgroundColor: theme.palette.grey[200],
+                color: theme.palette.getContrastText(theme.palette.grey[200]),
+            },        
+        }, theme.palette),
+        "&$primaryColor": makeColorStyle(theme.palette.primary, theme.palette),
+        "&$secondaryColor": makeColorStyle(theme.palette.secondary, theme.palette),
+        "&$informationColor": makeColorStyle(theme.palette.info, theme.palette),
+        "&$successColor": makeColorStyle(theme.palette.success, theme.palette),
+        "&$warningColor": makeColorStyle(theme.palette.warning, theme.palette),
+        "&$errorColor": makeColorStyle(theme.palette.error, theme.palette),
     },
+    defaultColor: {},    
+    primaryColor: {},
+    secondaryColor: {},
+    subtleColor: {},
+    informationColor: {},
+    successColor: {},
+    warningColor: {},
+    errorColor: {},
+    interactionFailed: {},
     pendingIndicator: {
         position: "absolute",
         visibility: "visible",
         display: "flex",
     },
 }));
+
+const makeColorStyle = (
+    color: PickedColor,
+    palette: Theme["palette"],
+) => decorateColorStyle(makeColorStyleCore(color, palette), palette);
+
+const decorateColorStyle = (style: Record<string, unknown>, palette: Theme["palette"]) => ({
+    ...style,
+    "&$interactionFailed": makeColorStyleCore(palette.error, palette),
+    "&.Mui-disabled": {
+        color: palette.action.disabled,
+        "&.MuiButton-outlined": {
+            borderColor: palette.action.disabledBackground,
+        },
+        "&.MuiButton-contained": {
+            backgroundColor: palette.action.disabledBackground,
+        },
+    }
+});
+
+type PickedColor = Pick<PaletteColor, "main" | "dark" | "contrastText">;
+
+const makeColorStyleCore = (color: PickedColor, palette: Theme["palette"]) => ({
+    color: color.main,
+    "&:hover": {
+        backgroundColor: alpha(color.main, palette.action.hoverOpacity),
+    },
+    "&.MuiButton-outlined": {
+        borderColor: alpha(color.main, 0.5),
+        "&:hover": {
+            borderColor: color.main,
+        },
+    },
+    "&.MuiButton-contained": {
+        backgroundColor: color.main,
+        color: color.contrastText,
+        "&:hover": {
+            backgroundColor: color.dark,
+        }
+    },
+});
